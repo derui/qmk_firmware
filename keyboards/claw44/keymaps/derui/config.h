0444cfe7b4805e9b0b9753a446dd3776e00d843a
derui <derutakayu@gmail.com>
2021-10-31 17:08:17 +0900

    fix: add valid options for comfortable typing

--- keyboards/claw44/keymaps/derui/config.h
+++ keyboards/claw44/keymaps/derui/config.h
@@ -19,3 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #define MASTER_LEFT
+
+#undef TAPPING_FORCE_HOLD
+#define TAPPING_TERM 150
+#define PERMISSIVE_HOLD
