068ca2c48f3ab72793654d2c8b1cea67c4a6ce20
derui <derutakayu@gmail.com>
2024-01-08 14:05:53 +0900

    fix: some tapping term for layer key to get esc key

--- keyboards/salicylic_acid3/7skb/keymaps/derui/keymap.c
+++ keyboards/salicylic_acid3/7skb/keymaps/derui/keymap.c
@@ -9,8 +9,8 @@
 #define KC_M_EISU M_EISU
 #define KC_LEISU M_LOWER_EISU
 #define KC_RKANA M_RAISE_KANA
-#define KC_LOWER MO(_LOWER)
-#define KC_RAISE MO(_RAISE)
+#define KC_LOWER LT(MO(_LOWER), KC_ESC)
+#define KC_RAISE LT(MO(_RAISE), KC_ESC)
 
 #ifdef RGBLIGHT_ENABLE
 //Following line allows macro to read current RGB settings
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------+--------|
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------+--------|
-     KC_LCTL,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, MO(_FN),
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
