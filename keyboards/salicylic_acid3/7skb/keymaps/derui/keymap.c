141dc9a2c73557bce738d1e6d9fd0444b3cbab13
derui <derutakayu@gmail.com>
2024-01-08 17:09:51 +0900

    fix: layout

--- keyboards/salicylic_acid3/7skb/keymaps/derui/keymap.c
+++ keyboards/salicylic_acid3/7skb/keymaps/derui/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, RSFT_T(KC_ESC), MO(_FN),
   //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------+--------|
-               KC_LGUI, KC_M_EISU,    KC_LOWER,   KC_SFT_SPC,       KC_SFT_ENT,  KC_RAISE,  KC_M_KANA, KC_RGUI 
+               KC_LGUI, KC_M_EISU,  KC_LOWER,   KC_SFT_SPC,  KC_SFT_ENT,  KC_RAISE,     KC_M_KANA, KC_RGUI 
           //`---------------------------------------------|   |--------------------------------------------'
   ),
 
