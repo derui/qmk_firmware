fadb3a107cb4af7937acbb8f2b7f7b7881d95edf
derui <derutakayu@gmail.com>
2024-01-13 09:37:54 +0900

    feat: do not use timer for holding checking

--- keyboards/salicylic_acid3/7skb/keymaps/derui/custom_key_handling.c
+++ keyboards/salicylic_acid3/7skb/keymaps/derui/custom_key_handling.c
@@ -1,28 +1,16 @@
 #include QMK_KEYBOARD_H
 #include "./custom_keycodes.h"
-#include "./next_util.h"
+#include "./custom_key_handling.h"
 
 /* provide default no-op handler */
 void der_nop(void) {}
 
 /* defined keys */
 
-void matrix_scan_user(void) {
-  for (int i = 0; i < all_defined_key_count;i++) {
-    custom_key_t* key = defined_keys[i];
-    
-    if (key->key_state == PRESSED) {
-      if (timer_elapsed(key->pressed_timer) > TAPPING_TERM) {
-        key->key_state = HOLDING;
-        key->on_start_holding();
-      }
-    }
-  }
-}
-
 bool process_record_derui(uint16_t keycode, keyrecord_t *record) {
   for (int i = 0; i < all_defined_key_count;i++) {
     custom_key_t* key = defined_keys[i];
+    
     if (key->custom_keycode == keycode) {
       if (record->event.pressed) {
           key->key_state = PRESSED;
@@ -40,9 +28,10 @@ bool process_record_derui(uint16_t keycode, keyrecord_t *record) {
       }
     } else {
       if (key->key_state == PRESSED && record->event.pressed) {
-        if (key->key_state == PRESSED) {
-          key->on_interrupted();
-        }
+        key->on_interrupted();
+
+        /* change state if it interrupted. */
+        key->key_state = HOLDING;
       }
     }
   }
