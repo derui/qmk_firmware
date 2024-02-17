7c9370cc5c1662ff623572141c3bb45ada3dfbd7
derui <derutakayu@gmail.com>
2024-02-17 12:25:00 +0900

    refactor: split custom key configurations

---
+++ keyboards/salicylic_acid3/7skb/keymaps/derui/layer.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_DERUI_LAYER_H
+#define CONFIG_DERUI_LAYER_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+enum layer_number {
+  _QWERTY = 0,
+  _MAC,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _FN,
+};
+
+#endif
