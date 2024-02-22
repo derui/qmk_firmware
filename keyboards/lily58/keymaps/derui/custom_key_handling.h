ab1ac0a55e528b1fa30c4640625bc4eed0697c48
derui <derutakayu@gmail.com>
2024-02-22 23:11:15 +0900

    feat: add original keymap and configurations for lily58

---
+++ keyboards/lily58/keymaps/derui/custom_key_handling.h
@@ -0,0 +1,33 @@
+#ifndef CONFIG_DERUI_NEXT_UTIL_H
+#define CONFIG_DERUI_NEXT_UTIL_H
+
+enum key_state {
+  PRESSED,
+  RELEASED,
+  HOLDING,
+};
+
+typedef struct {
+  enum custom_keycodes custom_keycode;
+  enum key_state key_state;
+  uint16_t pressed_timer;
+
+  /* invoke on key pressed */
+  void (* on_pressed)(void);
+  /* invoke on key tapped */
+  void (* on_tapped)(void);
+  /* invoke on interrupted other key pressed while this key pressed */
+  void (* on_interrupted)(void);
+  /* invoke on start hoding if the key is pressed without interrupted */
+  void (* on_start_holding)(void);
+  /* invoke on release holding key is released. Holding state is activated after interrupted */
+  void (* on_release_holding)(void);
+} custom_key_t;
+
+int all_defined_key_count;
+custom_key_t** defined_keys;
+
+void der_nop(void);
+bool process_record_derui(uint16_t keycode, keyrecord_t *record);
+
+#endif
---
+++ keyboards/lily58/keymaps/derui/custom_keycodes.h
@@ -0,0 +1,19 @@
+#ifndef CUSTOM_KEYCODES_DERUI_H
+#define CUSTOM_KEYCODES_DERUI_H
+
+enum custom_keycodes {
+  QWERTY = QK_USER,
+  M_EISU,
+  M_KANA,
+  RGB_RST,
+  M_CTLTB,
+  M_LOWER,
+  M_RAISE,
+  M_ENTER,
+  M_LSFTESC,
+  M_SPACE,
+  M_KANA_SHIFT,
+};
+
+#endif
+
