1dbc402c741183fcd8ccc459fbd0bbfedcd1fe72
derui <derutakayu@gmail.com>
2024-07-03 07:36:29 +0900

    feat: apply new key bindi

--- keyboards/lily58/keymaps/derui/rules.mk
+++ keyboards/lily58/keymaps/derui/rules.mk
@@ -16,8 +16,8 @@ SRC += ./layer_state_reader.c \
        ./logo_reader.c
 SRC += ./custom_key_handling.c
 #SRC += ./naginata.c
-# SRC += ./generated.c
-SRC += ./seq_map.c
+SRC += ./generated.c
+# SRC += ./seq_map.c
 SRC += ./custom_keys.c
 
         # ./lib/mode_icon_reader.c \
