d7c0e1f436d030847f4b9f876f17735598bbaefb
derui <derutakayu@gmail.com>
2024-11-17 10:05:32 +0900

    fix: merge key-maps in base layer

--- keyboards/lily58/keymaps/derui/layer.h
+++ keyboards/lily58/keymaps/derui/layer.h
@@ -6,7 +6,8 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 enum layer_number {
-  _QWERTY = 0,
+  _BASE = 0,
+  _QWERTY,
   _STURDY,
   _MAC,
   _LOWER,
