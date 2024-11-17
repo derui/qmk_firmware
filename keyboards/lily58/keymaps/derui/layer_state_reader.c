4a55a18f16a318d05f669ddc7eed7597f9252942
derui <derutakayu@gmail.com>
2024-11-17 10:05:32 +0900

    fix: merge key-maps in base layer

--- keyboards/lily58/keymaps/derui/layer_state_reader.c
+++ keyboards/lily58/keymaps/derui/layer_state_reader.c
@@ -1,14 +1,16 @@
 #include "quantum.h"
 #include <stdio.h>
 
-#define L_BASE 0
-#define L_STURDY (1 << 1)
-#define L_MAC (1 << 2)
-#define L_LOWER (1 << 3)
-#define L_RAISE (1 << 4)
-#define L_ADJUST (1 << 5)
+#define L_BASE 1
+#define L_QWERTY (1 << 1)
+#define L_STURDY (1 << 2)
+#define L_MAC (1 << 3)
+#define L_LOWER ((1 << 4) | 1)
+#define L_RAISE ((1 << 5) | 1)
+#define L_ADJUST ((1 << 6) | 1)
 #define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
 #define L_STURDY_MAC (L_STURDY | L_MAC)
+#define L_QWERTY_MAC (L_QWERTY | L_MAC)
 
 char layer_state_str[24];
 
@@ -16,9 +18,12 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: base");
+    break;
+  case L_QWERTY:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: qwerty");
     break;
-  case L_MAC:
+  case L_QWERTY_MAC:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: qwerty+mac");
     break;
   case L_STURDY:
